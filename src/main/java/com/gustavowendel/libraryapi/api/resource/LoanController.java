6a9de06c939c6b76798c2319d2a652a6bb5a7fcf
GustavoWendel <gustavo_wendel@hotmail.com>
2021-11-09 12:11:53 -0300

    Implementando Post de Empréstimos

--- src/main/java/com/gustavowendel/libraryapi/api/resource/LoanController.java
+++ src/main/java/com/gustavowendel/libraryapi/api/resource/LoanController.java
@@ -1,2 +1,37 @@
-package com.gustavowendel.libraryapi.api.resource;public class LoanController {
+package com.gustavowendel.libraryapi.api.resource;
+
+import com.gustavowendel.libraryapi.api.dto.LoanDTO;
+import com.gustavowendel.libraryapi.model.entity.Book;
+import com.gustavowendel.libraryapi.model.entity.Loan;
+import com.gustavowendel.libraryapi.service.BookService;
+import com.gustavowendel.libraryapi.service.LoanService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("api/loans")
+public class LoanController {
+
+    private final LoanService loanService;
+    private final BookService bookService;
+
+    public LoanController(LoanService loanService, BookService bookService) {
+        this.loanService = loanService;
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService.getBookByIsbn(dto.getIsbn()).get();
+        Loan entity = Loan.builder()
+                .book(book)
+                .customer(dto.getCustomer())
+                .loanDate(LocalDate.now()).build();
+
+        entity = loanService.save(entity);
+        return entity.getId();
+    }
 }
